6c7e1e40936e46efa32810fc6d03e43bc30a3d32
Paolo <p.erra1@studenti.unisa.it>
2021-08-03 08:38:42 +0200

    Recupero Password FIX

--- price-tracker-frontend/src/app/components/login/login.component.ts
+++ price-tracker-frontend/src/app/components/login/login.component.ts
@@ -6,25 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  resetPassword = false;
-  sendPassword = false;
+  
+  resetPassword = false; //Visualizza il form per il recupero password
+  sendPassword = false; //Visualizza il messaggio di invio password temporanea
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //Visualizzazione form login e per il reset password
   showResetPassword() {
     this.resetPassword = !this.resetPassword;
   }
 
+  //Invio password temporanea, e visualizzazione del messaggio
   sendPass() {
     this.sendPassword = true;
   }
 
-  showLogin() {
-    this.resetPassword = false;
-    this.sendPassword = true;
-  }
 }
