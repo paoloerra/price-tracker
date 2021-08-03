a3b58f5a155162425b3973cbec946b37c13fac50
RockstarBebi <antonioallegretta575@yahoo.com>
2021-08-03 10:20:30 +0200

    reset and update password

--- price-tracker-backend/src/main/java/com/example/demo/controller/UserController.java
+++ price-tracker-backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,25 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.User;
+import com.example.demo.payload.response.MessageResponse;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.EmailService;
 
 @RestController
 @RequestMapping("/user")
@@ -15,8 +29,13 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	@Qualifier("javasampleapproachMailSender")
+	private EmailService email;
+	
 	public UserController(UserRepository userRepository) {	
 		this.userRepository = userRepository;
 	}
+	
 
 }
--- price-tracker-backend/src/main/java/com/example/demo/service/EmailService.java
+++ price-tracker-backend/src/main/java/com/example/demo/service/EmailService.java
@@ -1,9 +1,13 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.models.User;
 
 
 @Component("javasampleapproachMailSender")
@@ -12,7 +16,7 @@ public class EmailService {
 	@Autowired
 	public JavaMailSender javaMailSender;
 
-	public void sendEmail(String email, String username) {
+	public void sendEmailSignUp(String email, String username) {
 
 		SimpleMailMessage msg = new SimpleMailMessage();
 
@@ -23,6 +27,26 @@ public class EmailService {
 		msg.setText("Hello " + username + "! You have succesfully signed up, search for your favourite products prices!");
 
 		javaMailSender.send(msg);
+	}
+	
+	public void sendEmailResetPass(Optional<User> utente, String temporaryPass) {
+		
+		SimpleMailMessage msg = new SimpleMailMessage();
+		
+		String userEmail = utente.get().getEmail();
+		
+		String username = utente.get().getUsername();
+		
+		
+		msg.setTo(userEmail);
+		
+		msg.setSubject("Dear " + username + ", Your new password is here");
+		
+		msg.setText("Hello " + username + ", this is your new temporary password " + temporaryPass + ", log and don't forget to change it.");
+
+		javaMailSender.send(msg);
+		
+		
 	}
 
 }
